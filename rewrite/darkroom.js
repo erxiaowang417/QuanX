78d058ddcbc7a202060a84c66a9bbd0561eb1418
erxiaowang417 <79679063+erxiaowang417@users.noreply.github.com>
2021-05-07 17:59:25 +0800

    Create darkroom.js

---
+++ rewrite/darkroom.js
@@ -0,0 +1,7 @@
+var body = 
+{ "is_valid_device" : true, 
+"has_valid_subscription":true,
+"expiration_date_ms" : 1800000000000,
+"is_table_resettable" : true, "subscription_product_id" : "com.kinemaster.sub.annual.ia2",
+ "state_code" : 0}; 
+$done({body:JSON.stringify(body)});
